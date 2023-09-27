9a23ecc3d262df60fc2e1eb846e04b26d41b6b7e
TomDiary <andihankai@gmail.com>
2023-09-27 21:39:24 +0800

    feat: 获取不到节点IP以及增加语言类型

--- main.sh
+++ main.sh
@@ -1,9 +1,14 @@
 #!/usr/bin/env bash
+# @author TomDiary
+# @lastTime 2023-09-27 21:39
+# @version 0.1.0
 
 # 全局变量
 WORK_DIR="/tmp/.besttrace/" # 工作目录
-SHELL_VERSION="0.0.1" # shell 脚本
+SHELL_VERSION="0.1.0" # shell 脚本
 IS_DEBUG=0 # 是否开启 DEBUG（0-关闭，1-开启）
+ADDRESS_LIST=""
+BESTTRACE_SITE=""
 
 # 字体颜色
 FONT_BLACK="\033[30m"
@@ -23,6 +28,8 @@ MSG_DEG="${FONT_PURPLE} [Debug] ${FONT_SUFFIX}"
 MSG_ERROR="${FONT_RED} [Error] ${FONT_SUFFIX}"
 MSG_SUCCESS="${FONT_GREEN} [Success] ${FONT_SUFFIX}"
 MSG_FAIL="${FONT_SKYBLUE} [Failed] ${FONT_SUFFIX}"
+
+keys_order=(0 1 2 3 4 5 6 7 8 9)
 
 declare -A ip_address=(
   ['0']="219.141.136.12"
@@ -64,10 +71,22 @@ declare -A ip_address_cn=(
 )
 
 basic_init() {
-  if [ -n $1 ]; then
-    lang=$1
+  if [ -z "$BESTTRACE_LANG" ] || [ "$BESTTRACE_LANG" != "cn" ]; then
+    BESTTRACE_LANG="en"
+    ADDRESS_LIST="ip_address_en"
   else
-    lang="en"
+    ADDRESS_LIST="ip_address_cn"
+  fi
+
+  if [ -z "$BESTTRACE_SOURCE" ] || [ "$BESTTRACE_SOURCE" != "ipip" ]; then
+    BESTTRACE_SOURCE="github"
+    BESTTRACE_SITE="https://raw.githubusercontent.com/tomdiary/besttrace/main/besttrace4linux.zip"
+  else
+    BESTTRACE_SITE="https://cdn.ipip.net/17mon/besttrace4linux.zip"
+  fi
+
+  if [ -z "$BESTTRACE_QUERIES" ]; then
+    BESTTRACE_QUERIES=1
   fi
   
   if [ ! -d $WORK_DIR ]; then
@@ -121,8 +140,7 @@ basic_init() {
     echo -e "${MSG_INFO}Installing Besttrace ..."
     # https://raw.githubusercontent.com/tomdiary/besttrace/main/besttrace4linux.zip
     # https://cdn.ipip.net/17mon/besttrace4linux.zip
-    wget --no-check-certificate -O "besttrace.zip" https://cdn.ipip.net/17mon/besttrace4linux.zip
-    # wget --no-check-certificate -O "besttrace.zip" https://raw.githubusercontent.com/tomdiary/besttrace/main/besttrace4linux.zip
+    wget --no-check-certificate -O "besttrace.zip" "$BESTTRACE_SITE"
     unzip "besttrace.zip" -d "${WORK_DIR}"
     chmod +x "${WORK_DIR}besttrace"
   fi
@@ -138,19 +156,48 @@ clear_besttrace() {
   rm -rf $WORK_DIR
 }
 
+get_address_value() {
+  local index=$1
+  eval "echo \${${ADDRESS_LIST}[$index]}"
+}
+
+about() {
+	echo ""
+	echo " ========================================================= "
+	echo " \    Besttrace https://github.com/tomdiary/besttrace    / "
+	echo " \                 Lang, Souce, Queries                  / "
+	echo " \                  v0.1.0  2023-08-27                   / "
+	echo " \                   Author: TomDiary                    / "
+	echo " ========================================================= "
+	echo ""
+}
+
 main() {
   basic_init
-
   clear
+  about
   next
 
-  for key in "${!ip_address_cn[@]}"; do
-    echo -e "${ip_address_cn[$key]}"
-    "${WORK_DIR}besttrace" -g ${lang} -q 1 ${ip_address[$key]}
+  for key in "${keys_order[@]}"; do
+    echo -e "$(get_address_value $key)"
+    "${WORK_DIR}besttrace" -g ${BESTTRACE_LANG} -q ${BESTTRACE_QUERIES} ${ip_address["$key"]}
     next
   done
 
   clear_besttrace
 }
 
+while getopts l:b:q: option
+do 
+  case $option in
+    l)
+      BESTTRACE_LANG=${OPTARG};;
+    b)
+      BESTTRACE_SOURCE=${OPTARG};;
+    q)
+      BESTTRACE_QUERIES=${OPTARG};;
+  esac
+done
+
 main
+
